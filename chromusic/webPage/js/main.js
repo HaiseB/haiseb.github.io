069272df3260aa4c80bf275bbeae15e290c4f2a2
Benjamin HAISE <43814190+HaiseB@users.noreply.github.com>
2024-01-20 23:55:51 +0100

    test 2 with code deletions

--- chromusic/webPage/js/main.js
+++ chromusic/webPage/js/main.js
@@ -1,15 +1,5 @@
-async function getSelectedTheme() {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(['selectedTheme'], function (result) {
-            resolve(result.selectedTheme);
-            console.log("Theme retrieved : "+result.selectedTheme)
-        });
-    });
-}
-
 async function start() {
-    const selectedTheme = (await getSelectedTheme()) || 'vapor';
-    const cssFileName = `${selectedTheme}.bootstrap.min.css`;
+    const cssFileName = `vapor.bootstrap.min.css`;
     const cssFilePath = `../bootstrap/${cssFileName}`;
 
     const styleLink = document.createElement('link');
