68dedacbe28419b3c7808c546578ca31cf6b9ddc
Benjamin HAISE <benjaminhaise@gmail.com>
2024-02-07 09:19:33 +0100

    Mise en place du template + de la récupération des cadaux du google sheet

--- module/babylist/main.js
+++ module/babylist/main.js
@@ -1,5 +1,4 @@
-// Charger le client Google Sheets
-/*gapi.load('client', initClient);
+gapi.load('client', initClient);
 
 function initClient() {
     gapi.client.init({
@@ -8,43 +7,12 @@ function initClient() {
     }).then(function() {
         console.log('Client Google Sheets chargé avec succès.');
 
-        // Vous pouvez maintenant appeler les fonctions pour lire et écrire des données dans Google Sheets
-        // Par exemple :
         getDataFromGoogleSheets();
-        createCommentInGoogleSheets();
     }).catch(function(error) {
         console.error('Erreur lors du chargement du client Google Sheets:', error);
     });
-}*/
-
-
-// Charger le client OAuth2 de Google
-gapi.load('auth2', initAuth2);
-
-function initAuth2() {
-    gapi.auth2.init({
-        client_id: '128823670908-fvr3fsmaqes6ibqtqep5h1s2rr4uk5tu.apps.googleusercontent.com',
-    }).then(function() {
-        console.log('Client OAuth2 chargé avec succès.');
-        // Vous pouvez maintenant appeler les fonctions pour obtenir un jeton d'accès
-        getDataFromGoogleSheets();
-    }).catch(function(error) {
-        console.error('Erreur lors du chargement du client OAuth2:', error);
-    });
 }
 
-// Connexion avec Google pour obtenir un jeton d'accès
-function signInWithGoogle() {
-    gapi.auth2.getAuthInstance().signIn().then(function() {
-        console.log('Connecté avec succès.');
-        // Une fois connecté, vous pouvez utiliser le jeton d'accès pour accéder à l'API Google Sheets
-        // Par exemple : getDataFromGoogleSheets();
-    }).catch(function(error) {
-        console.error('Erreur lors de la connexion avec Google:', error);
-    });
-}
-
-// Lecture des données depuis Google Sheets
 function getDataFromGoogleSheets() {
     const sheetId = '1VFepd02PXQJfIXfL22oEnP6XJgdWIEZLRF3NIw4dIyE';
     const range = 'cadeaux!A:E'; // Plage de données à lire
@@ -62,67 +30,5 @@ function getDataFromGoogleSheets() {
         }
     }, function(response) {
         console.error('Erreur lors de la lecture des données:', response.result.error.message);
-    });
-}
-
-// Écriture de données dans Google Sheets
-function writeToGoogleSheets() {
-    const sheetId = '1VFepd02PXQJfIXfL22oEnP6XJgdWIEZLRF3NIw4dIyE';
-    const range = 'cadeaux!A:E'; // Plage de données à écrire
-
-    var values = [
-        // Les données que vous souhaitez écrire dans la feuille de calcul
-        ["Nouveau cadeau", 10, "lien_image", "Propriétaire", "lien_cadeau"],
-    ];
-
-    var body = {
-        values: values
-    };
-
-    gapi.client.sheets.spreadsheets.values.append({
-        spreadsheetId: sheetId,
-        range: range,
-        valueInputOption: 'USER_ENTERED',
-        resource: body
-    }).then((response) => {
-        var result = response.result;
-        console.log(`${result.updates.updatedCells} cellules mises à jour.`);
-    }, function(response) {
-        console.error('Erreur lors de l\'écriture des données:', response.result.error.message);
-    });
-}
-
-function createCommentInGoogleSheets() {
-    const sheetId = '1VFepd02PXQJfIXfL22oEnP6XJgdWIEZLRF3NIw4dIyE';
-    const range = 'cadeaux!A1'; // Cellule où vous souhaitez ajouter le commentaire
-
-    const request = {
-        spreadsheetId: sheetId,
-        resource: {
-            requests: [
-                {
-                    "addComment": {
-                        "location": {
-                            "sheetId": 0, // L'ID de la feuille de calcul où vous souhaitez ajouter le commentaire
-                            "dimensionRange": {
-                                "sheetId": 0,
-                                "dimension": "COLUMNS",
-                                "startIndex": 0,
-                                "endIndex": 1
-                            }
-                        },
-                        "text": "Votre commentaire ici"
-                    }
-                }
-            ]
-        }
-    };
-
-    gapi.client.sheets.spreadsheets.batchUpdate(request)
-    .then(function(response) {
-        console.log('Commentaire créé avec succès.');
-    })
-    .catch(function(error) {
-        console.error('Erreur lors de la création du commentaire:', error.result.error.message);
     });
 }
